b5a5e7353745460fe602f07b77336a823cbd0732
estelle-dupuis <etelle.dupuis@laplateforme.io>
2025-01-14 16:19:40 +0100

    fin des jobs

---
+++ Jour-2/job-05.php
@@ -0,0 +1,19 @@
+<?php
+function estPremier($nombre) {
+    if ($nombre <= 1) {
+        return false; 
+    }
+    for ($i = 2; $i <= sqrt($nombre); $i++) {
+        if ($nombre % $i === 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+
+for ($i = 2; $i <= 1000; $i++) {
+    if (estPremier($i)) {
+        echo "$i<br>"; 
+    }
+}
+?>
